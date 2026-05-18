8af73d4183d4aea2725915a95e4a19ad54f05456
Guille4618 <llebresguillermo@gmail.com>
2026-05-18 10:16:47 +0200

    fix: CORS actualizado para producción

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:5173" },
+          { key: "Access-Control-Allow-Origin", value: "https://learning-inventory-frontend.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type" },
         ],
